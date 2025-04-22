782f9cf870fa30269380a71cbb857a2bd7598688
datonecoolguy <oskotheguy@gmail.com>
2025-04-23 00:19:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
-// Fungsi untuk menampilkan halaman formulir
 function showFormPage() {
   document.getElementById('landing').style.display = 'none';
   document.getElementById('formPage').classList.add('active');
 }
 
-// Fungsi untuk menangani pengiriman formulir
 function handleSubmit(event) {
   event.preventDefault();
   document.querySelector('form').style.display = 'none';
   document.getElementById('thankYouMessage').style.display = 'block';
 
-  // Simpan data formulir ke localStorage
-  const formData = {
+  // Simpan data ke localStorage
+  localStorage.setItem('formData', JSON.stringify({
     title: document.getElementById('title').value,
     description: document.getElementById('desc').value,
-    author: document.getElementById('author').value,
-    file: document.getElementById('file').files[0].name
-  };
-
-  localStorage.setItem('formData', JSON.stringify(formData));
+    author: document.getElementById('author').value
+  }));
 
   // Tampilkan opsi setelah 3 detik
   setTimeout(() => {
     document.getElementById('thankYouMessage').style.display = 'none';
     document.getElementById('optionPage').classList.add('active');
-  }, 3000); // Setelah 3 detik
+  }, 3000);
 }
 
-// Fungsi untuk menyimpan pilihan jenis karya ke localStorage
 function saveOption(option) {
-  const formData = JSON.parse(localStorage.getItem('formData')) || {};
-  formData.option = option;
-  
-  // Simpan pilihan dan data formulir ke localStorage
-  localStorage.setItem('formData', JSON.stringify(formData));
+  // Simpan pilihan ke localStorage
+  localStorage.setItem('option', option);
 
   // Kembali ke halaman utama setelah 3 detik
   setTimeout(() => {
